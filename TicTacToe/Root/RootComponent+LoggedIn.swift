9efab65563b8ad33c863672491a619077759cb10
woongs <ccw1021@naver.com>
2021-06-25 15:36:35 +0900

    RootComponent에 LoggedIn 확장
    View는 임시로 처리해둠

---
+++ TicTacToe/Root/RootComponent+LoggedIn.swift
@@ -0,0 +1,25 @@
+//
+//  RootComponent+LoggedIn.swift
+//  TicTacToe
+//
+//  Created by woong on 2021/06/25.
+//  Copyright © 2021 Uber. All rights reserved.
+//
+
+import RIBs
+
+/// The dependencies needed from the parent scope of Root to provide for the LoggedIn scope.
+// TODO: Update RootDependency protocol to inherit this protocol.
+protocol RootDependencyLoggedIn: Dependency {
+    // TODO: Declare dependencies needed from the parent scope of Root to provide dependencies
+    // for the LoggedIn scope.
+}
+
+extension RootComponent: LoggedInDependency {
+    
+    var LoggedInViewController: LoggedInViewControllable {
+        return RootViewController()
+    }
+}
+
+extension RootViewController: LoggedInViewControllable { }
