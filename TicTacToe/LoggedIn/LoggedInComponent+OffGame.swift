6235498ae8b2a2ca7ba96d29e7a286a949888b3e
woongs <ccw1021@naver.com>
2021-06-25 23:11:37 +0900

    LoggedIn에 OffGameDependency 확장

---
+++ TicTacToe/LoggedIn/LoggedInComponent+OffGame.swift
@@ -0,0 +1,20 @@
+//
+//  LoggedInComponent+OffGame.swift
+//  TicTacToe
+//
+//  Created by woong on 2021/06/25.
+//  Copyright © 2021 Uber. All rights reserved.
+//
+
+import RIBs
+
+/// The dependencies needed from the parent scope of LoggedIn to provide for the OffGame scope.
+// TODO: Update LoggedInDependency protocol to inherit this protocol.
+protocol LoggedInDependencyOffGame: Dependency {
+    // TODO: Declare dependencies needed from the parent scope of LoggedIn to provide dependencies
+    // for the OffGame scope.
+}
+
+extension LoggedInComponent: OffGameDependency {
+    
+}
